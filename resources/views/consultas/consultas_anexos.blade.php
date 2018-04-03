d65a4ff028867cd31dfa51728b9bfd5623924dde
Gerardo Gómez <gerardo.gomr@gmail.com>
2018-04-02 22:49:07 -0500

    Se elimina botón para eliminar anexo y se pinta de otro color en vista consultas

--- resources/views/consultas/consultas_anexos.blade.php
+++ resources/views/consultas/consultas_anexos.blade.php
@@ -13,8 +13,8 @@
                             <tr>
                                 <td>{!! $anexo->nombreFormal() !!}</td>
                                 <td>
-                                    <a href="{{ url('pacientes/anexos/ver/' . $expediente->getId() . '/' . $anexo->nombre()) }}" target="_blank" class="btn btn-default" data-toggle="tooltip" data-original-title="Ver anexo"><i class="fa fa-search"></i></a>
-                                    <button type="button" class="eliminarAnexo btn btn-default" data-url="{{ url('pacientes/anexos/eliminar') }}" data-id="{{ base64_encode($anexo->nombre()) }}" data-toggle="tooltip" data-original-title="Eliminar anexo"><i class="fa fa-times"></i></button></td>
+                                    <a href="{{ url('pacientes/anexos/ver/' . $expediente->getId() . '/' . $anexo->nombre()) }}" target="_blank" class="btn btn-primary" data-toggle="tooltip" data-original-title="Ver anexo"><i class="fa fa-search"></i></a>
+                                </td>
                             </tr>
                         @endforeach
                     </tbody>
